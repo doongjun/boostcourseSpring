43715bfca4e1ac53d39cba437fd534fda165e68b
doongjun <doongjun@kakao.com>
2021-08-09 15:41:11 +0900

    상태유지기술(Cookie)

--- guestbook/src/main/java/kr/or/connect/guestbook/dao/GuestbookDaoSqls.java
+++ guestbook/src/main/java/kr/or/connect/guestbook/dao/GuestbookDaoSqls.java
@@ -4,4 +4,4 @@ public class GuestbookDaoSqls {
 	public static final String SELECT_PAGING = "SELECT id, name, content, regdate FROM guestbook ORDER BY id DESC LIMIT :start, :limit";
 	public static final String DELETE_BY_ID = "DELETE FROM guestbook WHERE id = :id";
 	public static final String SELECT_COUNT = "SELECT COUNT(*) FROM guestbook";
-}
+} 
