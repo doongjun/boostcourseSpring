b893653b018be76df345f177a1285fbe79331cd1
doongjun <doongjun.kim@gmail.com>
2021-08-23 18:42:09 +0900

    Spring Security DB 로그인

--- securityexam/src/main/java/org/edwith/webbe/securityexam/service/MemberServiceImpl.java
+++ securityexam/src/main/java/org/edwith/webbe/securityexam/service/MemberServiceImpl.java
@@ -3,22 +3,41 @@ package org.edwith.webbe.securityexam.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.edwith.webbe.securityexam.dao.MemberDao;
+import org.edwith.webbe.securityexam.dao.MemberRoleDao;
+import org.edwith.webbe.securityexam.dto.Member;import org.edwith.webbe.securityexam.dto.MemberRole;
 import org.edwith.webbe.securityexam.service.security.UserEntity;
 import org.edwith.webbe.securityexam.service.security.UserRoleEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberServiceImpl implements MemberService {
-
-	@Override
-	public UserEntity getUser(String loginUserId) {
-		return new UserEntity("dongjun", "$2a$10$mylS4kmVTQK6mT8LWjVeru6zeihtTvS74HPf0BCQ1Cm0DDpWP9NpS");
+	private MemberDao memberDao;
+	private MemberRoleDao memberRoleDao;
+	
+	public MemberServiceImpl(MemberDao memberDao, MemberRoleDao memberRoleDao) {
+		this.memberDao = memberDao;
+		this.memberRoleDao = memberRoleDao;
 	}
 
 	@Override
+	@Transactional
+	public UserEntity getUser(String loginUserId) {
+		Member member = memberDao.getMemberByEmail(loginUserId);
+		return new UserEntity(member.getEmail(), member.getPassword());
+	}
+
+	@Override
+	@Transactional
 	public List<UserRoleEntity> getUserRoles(String loginUserId) {
+		List<MemberRole> memberRoles = memberRoleDao.getRolesByEmail(loginUserId);
 		List<UserRoleEntity> list = new ArrayList<>();
-		list.add(new UserRoleEntity("dongjun", "ROLE_USER"));
+		
+		for(MemberRole memberRole : memberRoles) {
+			list.add(new UserRoleEntity(loginUserId, memberRole.getRoleName()));
+		}
+
 		return list;
 	}
 
