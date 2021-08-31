765b3e89d67911dc1021aee9df4d5f414f09947f
doongjun <doongjun@kakao.com>
2021-08-31 20:23:01 +0900

    ArgumentResolver

---
+++ guestbook/src/main/java/kr/or/connect/guestbook/argumentresolver/HeaderMapArgumentResolver.java
@@ -0,0 +1,39 @@
+package kr.or.connect.guestbook.argumentresolver;
+
+import java.util.Iterator;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class HeaderMapArgumentResolver implements HandlerMethodArgumentResolver {
+	
+	// 호출되는 Controller의 Parameter를 검사하여 CustomArgumentResolver의 로직을 수행할 지를 검사.
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.getParameterType() == HeaderInfo.class;
+	}
+	
+	// suooirtsParameter가 true인 파라미터에 대해 어떤 값을 넣어줄 것인지에 대한 로직 수행
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		
+		HeaderInfo headerInfo = new HeaderInfo();
+		
+		Iterator<String> headerNames = webRequest.getHeaderNames();
+		while(headerNames.hasNext()) {
+			String headerName = headerNames.next();
+			String headerValue = webRequest.getHeader(headerName);
+			
+			System.out.println(headerName + ", " + headerValue);
+			
+			headerInfo.put(headerName, headerValue);
+		}
+		
+		return headerInfo;
+	}
+
+}
